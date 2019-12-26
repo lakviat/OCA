2723a55ec06e7ddea9d5340b5b7571bd55dbe0d7
admin <admin@localhost.com>
2019-12-26 16:12:58 -0600

    Adding Tests i completed

--- src/main/java/findMax.java
+++ src/main/java/findMax.java
@@ -2,5 +2,6 @@ public class findMax {
     public static void main(String[] args) {
         int [] number = {10,20,30,40,50,60,};
         //int max = findMax(number);
+        //findMax runs frist because actually when calling this method to Main Method the findMax method will be executed
     }
 }
